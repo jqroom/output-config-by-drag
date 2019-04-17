16815580933b515ba3b53c3f3ed006e8a7d5ea64
jiangqi <jqroom.com@gmail.com>
2019-04-17 18:35:18 +0800

    feat：增加删除功能

---
+++ js/icons.js
@@ -0,0 +1,12 @@
+const myIcons = {
+    // 垃圾箱
+    trashcan: `<svg height="16" class="octicon octicon-trashcan" viewBox="0 0 12 16" version="1.1" width="12" aria-hidden="true"><path fill-rule="evenodd" d="M11 2H9c0-.55-.45-1-1-1H5c-.55 0-1 .45-1 1H2c-.55 0-1 .45-1 1v1c0 .55.45 1 1 1v9c0 .55.45 1 1 1h7c.55 0 1-.45 1-1V5c.55 0 1-.45 1-1V3c0-.55-.45-1-1-1zm-1 12H3V5h1v8h1V5h1v8h1V5h1v8h1V5h1v9zm1-10H2V3h9v1z"></path></svg>`,
+    // 空心下箭头
+    chevronDown: `<svg height="16" class="octicon octicon-chevron-down" viewBox="0 0 10 16" version="1.1" width="10" aria-hidden="true"><path fill-rule="evenodd" d="M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6l-5 5z"></path></svg>`,
+    // 实心右箭头
+    triangleRight: `<svg height="16" class="octicon octicon-triangle-right" viewBox="0 0 6 16" version="1.1" width="6" aria-hidden="true"><path fill-rule="evenodd" d="M0 14l6-6-6-6v12z"></path></svg>`,
+    // 空心右箭头
+    chevronRight: `<svg height="16" class="octicon octicon-chevron-right" viewBox="0 0 8 16" version="1.1" width="8" aria-hidden="true"> <path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3l5 5z"></path></svg>`,
+    // 三横线
+    threeBars: `<svg height="16" class="octicon octicon-three-bars" viewBox="0 0 12 16" version="1.1" width="8" aria-hidden="true"> <path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path> </svg>`
+};
--- js/index.js
+++ js/index.js
@@ -1,23 +1,22 @@
-let downTpl = `<svg height="16" class="octicon octicon-chevron-down" viewBox="0 0 10 16" version="1.1" width="10" aria-hidden="true"><path fill-rule="evenodd" d="M5 11L0 6l1.5-1.5L5 8.25 8.5 4.5 10 6l-5 5z"></path></svg>`;
-let rightTpl = `<svg height="16" class="octicon octicon-chevron-right" viewBox="0 0 8 16" version="1.1" width="8" aria-hidden="true"> <path fill-rule="evenodd" d="M7.5 8l-5 5L1 11.5 4.75 8 1 4.5 2.5 3l5 5z"></path></svg>`;
 let tpl = `
 <div class="form-group">
     <div class="input-group mb-3">
         <div class="input-group-prepend">
             <button class="btn btn-outline-secondary collapseBtn" type="button">
-                ${ downTpl }
+                ${ myIcons.chevronDown }
             </button>
         </div>
         <input type="text" class="form-control" placeholder="请输入规则内容" aria-label="title">
         <div class="input-group-append">
             <button class="btn btn-outline-secondary addSiblings" type="button">+兄弟</button>
             <button class="btn btn-outline-secondary addChilds" type="button">+孩子</button>
-            <span class="input-group-text dragBtn">
-                <svg height="16" class="octicon octicon-three-bars" viewBox="0 0 12 16" version="1.1" width="8" aria-hidden="true"> <path fill-rule="evenodd" d="M11.41 9H.59C0 9 0 8.59 0 8c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zm0-4H.59C0 5 0 4.59 0 4c0-.59 0-1 .59-1H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1h.01zM.59 11H11.4c.59 0 .59.41.59 1 0 .59 0 1-.59 1H.59C0 13 0 12.59 0 12c0-.59 0-1 .59-1z"></path> </svg>
-            </span>
+            <button data-toggle="modal" data-target="#deleteNodePop" class="btn btn-outline-secondary" type="button">${ myIcons.trashcan }</button>
+            <span class="input-group-text dragBtn">${ myIcons.threeBars }</span>
         </div>
     </div>
 </div>`;
+
+let $deleteTag = '';
 
 function init() {
     bindNodeEvents();
@@ -36,10 +35,10 @@ function bindNodeEvents() {
         let $closestFormGroup = $this.parents('.form-group').eq(0);
         let curTpl = tpl;
         if ($closestFormGroup.find('>.childContent').size() <= 0) {
-            curTpl = `<div class="childContent ml-3 collapse show">${ tpl }</div>`
+            curTpl = `<div class="childContent ml-3 collapse show">${ curTpl }</div>`
         } else {
             $closestFormGroup = $closestFormGroup.find('>.childContent').addClass('show');
-            $closestFormGroup.find('>.collapseBtn').html(downTpl);
+            $closestFormGroup.find('>.collapseBtn').html(myIcons.chevronDown);
         }
         $closestFormGroup.append(curTpl);
     }).on('mouseenter', '.dragBtn', function () {
@@ -53,11 +52,22 @@ function bindNodeEvents() {
 
         if ($toggleTarget.hasClass('show')) {
             $toggleTarget.collapse('hide');
-            $this.html(rightTpl);
+            $this.html(myIcons.triangleRight);
         } else {
             $toggleTarget.collapse('show');
-            $this.html(downTpl);
+            $this.html(myIcons.chevronDown);
         }
+    });
+
+    $('#deleteNodePop').on('show.bs.modal', function(event){
+        let $this = $(event.relatedTarget);
+        let $closestFormGroup = $this.parents('.form-group').eq(0);
+        $deleteTag = $closestFormGroup.addClass('delete-shadow');
+    }).on('hide.bs.modal', function(event){
+        $deleteTag.removeClass('delete-shadow');
+    }).on('click', '.deleteBtn', function () {
+        $('#deleteNodePop').modal('hide')
+        $deleteTag.remove();
     });
 }
 
@@ -89,13 +99,12 @@ function bindDragEvents() {
         $leaveTag.removeClass('shadow');
     }).on('dragend', function (event) {
         // 重置透明度
+        $('.shadow').removeClass('shadow');
         $dragged.attr('draggable', "").css('opacity', '');
     }).on('drop', function (event) {
         // 阻止默认动作（如打开一些元素的链接）
         event.preventDefault();
         let $curTarget = $getCurTarget(event);
-
-        $curTarget.removeClass('shadow');
 
         if ($curTarget.is($dragged) || $.contains($dragged[0], $curTarget[0])) {
             return false;
@@ -120,7 +129,8 @@ function $getCurTarget(target) {
 }
 
 function getCurNavIndex(target, type) {
-    let levelArr = [$(target).index() + 1];
+    let $target = $(target);
+    let levelArr = [$target.index() + 1];
     let $parensFormGroup = $(target).parents('.form-group');
     $parensFormGroup.each((index, value) => {
         levelArr.push($(value).index() + 1);
@@ -128,14 +138,20 @@ function getCurNavIndex(target, type) {
 
     if (type == 'addSibling') {
         levelArr.pop();
-        levelArr.push($(target).siblings().size() + 2); // 漏了自己和新增的那个，所以要加2
+        levelArr.push($target.siblings().size() + 2); // 漏了自己和新增的那个，所以要加2
+    } else if (type == 'addChild') {
+        levelArr.push($target.find('>.childContent').find('>.form-group').size() + 1); // 漏了自己和新增的那个，所以要加2
     }
 
-    return levelArr.reverse();
+    return levelArr;
 }
 
 function getFormatSelector(arr) {
     return `.form-group:eq(${ arr.reduce((sum, cur) => sum + cur, 0) - 1 }) > .childContent`;
+}
+
+function getType(obj) {
+    return Object.prototype.toString.call(obj).slice(8, -1).toLocaleLowerCase();
 }
 
 init();
